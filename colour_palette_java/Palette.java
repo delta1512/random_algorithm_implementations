68f7d6c4a7df9f368f2f85573ac1dc9f288d24bc
delta1512 <markbel555@gmail.com>
2017-06-02 19:01:25 +1000

    Fixed commenting

--- colour_palette_java/Palette.java
+++ colour_palette_java/Palette.java
@@ -7,12 +7,12 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class Palette {
-  int thresh = 50;
-  int[] imageData; //load colour data
+  int thresh = 50; //defines the maximum difference in pixel values the program will detect
+  int[] imageData; //initialise the variable to hold the raw image data
   ArrayList<Pixel> hist = new ArrayList<Pixel>(); //initialise the histogram
   public Palette(String file){
     try {
-      BufferedImage image = ImageIO.read(new File(file)); //loads image into a buffer
+      BufferedImage image = ImageIO.read(new File(file)); //load image into a buffer
       int w = image.getWidth();
       int h = image.getHeight();
       if (w*h > 40000){ //if the image is bigger than 200x200...
@@ -27,9 +27,9 @@ public class Palette {
         h = image.getHeight();
       }
       imageData = image.getRGB(0, 0, w, h, null, 0, w);
-      for (int i = 0; i < imageData.length; i++){
+      for (int i = 0; i < imageData.length; i++){ //loop that finds unique pixels and gathers frequency data
         Color tmp = new Color(imageData[i]); //initialise a new Pixel
-        boolean found = false; //set up the search result
+        boolean found = false;
         for (Pixel pix : hist){ //search the histogram...
           if (pix.compare(tmp)){ //for the same colours
             found = true;
@@ -45,28 +45,27 @@ public class Palette {
 
   public short[] getColour(){
     //int freqResult; //debug
-    ArrayList<Pixel> record = new ArrayList<Pixel>(); //set up a temporary record for analysis
+    ArrayList<Pixel> record = new ArrayList<Pixel>(); //set up a temporary record to hold similar pixels
     //Pixel comparison = hist.get(0); //get the pixel to compare with
     Pixel highestFreq = hist.get(0);
-    for (Pixel pix : hist){
+    for (Pixel pix : hist){ //find the pixel with the highest frequency
       if (pix.frequency > highestFreq.frequency){
         highestFreq = pix;
       }
     }
-    Pixel comparison = highestFreq;
+    Pixel comparison = highestFreq; //make it the comparison pixel
     record.add(comparison); //add it to the record
     for (int i = 0; i < hist.size(); i++){ //find all the pixels that are similar to the comparison one...
       Pixel pix = hist.get(i);
       if (comparison.findEuclideanDist(pix.RGBarr) < thresh){ //based on the euclidean distance threshold
-        record.add(pix); //add it to the record
+        record.add(pix); //if it's similar, add it to the record
       }
     }
-    //set up sum variables
     //int sum = 0; //debug
     int[] sumPix = {0, 0, 0};
     for (Pixel pix : record){
       //sum += pix.frequency; //debug
-      for (int i = 0; i < 3; i++){ //sum the pixel values
+      for (int i = 0; i < 3; i++){ //add the values of each RGB to the sum
         sumPix[i] += pix.RGBarr.get(i);
       }
       for (int i = 0; i < hist.size(); i++){ //remove the pixel from the main histogram
@@ -82,6 +81,6 @@ public class Palette {
     }
     //freqResult = av; //debug
     //System.out.println(freqResult); //debug
-    return avPix;
+    return avPix; //send out the picked colour
   }
 }
